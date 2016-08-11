298c15bf5ca0b0295a6762bc49de79a6885d8bbc
Le Minh Tuan <mtle10@dyn-43-246-235-194.its.monash.edu.au>
2016-08-11 19:01:54 +1000

    first commit

---
+++ FirstClass.h
@@ -0,0 +1,16 @@
+//
+//  FirstClass.h
+//  learning ios
+//
+//  Created by Le Minh Tuan on 8/08/2016.
+//  Copyright © 2016 Le Minh Tuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstClass : NSObject
+
+@property (nonatomic) int age;
+
+
+@end
---
+++ FirstClass.m
@@ -0,0 +1,13 @@
+//
+//  FirstClass.m
+//  learning ios
+//
+//  Created by Le Minh Tuan on 8/08/2016.
+//  Copyright © 2016 Le Minh Tuan. All rights reserved.
+//
+
+#import "FirstClass.h"
+
+@implementation FirstClass
+
+@end
